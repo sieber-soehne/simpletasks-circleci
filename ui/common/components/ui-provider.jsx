64791efb357172fd6a5755f3ee9bb39380b6f65a
sieber-soehne <kilian.sieber@kabelplus.at>
2024-06-02 11:33:31 +0200

    first commit

--- ui/common/components/ui-provider.jsx
+++ ui/common/components/ui-provider.jsx
@@ -3,24 +3,38 @@ import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react';
 
 const customTheme = extendTheme({
   config: {
-    initialColorMode: 'dark',
+    initialColorMode: 'light',
     useSystemColorMode: false,
   },
-});
-
-const toastOptions = {
-  defaultOptions: {
-    position: 'top-right',
-    duration: 5000,
-    isClosable: true,
+  styles: {
+    global: (props) => ({
+      body: {
+        bg: props.colorMode === 'dark' ? 'gray.800' : 'white',
+        color: props.colorMode === 'dark' ? 'white' : 'black',
+      },
+    }),
   },
-};
+  colors: {
+    brand: {
+      50: '#f5f7ff',
+      100: '#e0e7ff',
+      200: '#c7d2fe',
+      300: '#a5b4fc',
+      400: '#818cf8',
+      500: '#6366f1',
+      600: '#4f46e5',
+      700: '#4338ca',
+      800: '#3730a3',
+      900: '#312e81',
+    },
+  },
+});
 
 export function UIProvider({ children }) {
   return (
     <>
       <ColorModeScript initialColorMode={customTheme.config.initialColorMode} />
-      <ChakraProvider theme={customTheme} toastOptions={toastOptions}>
+      <ChakraProvider theme={customTheme}>
         {children}
       </ChakraProvider>
     </>
